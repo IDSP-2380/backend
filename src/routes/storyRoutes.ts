3489334763682db609cf2b9d660bd61b71ff9df0
mayaarafa <marafa@my.bcit.ca>
2025-04-29 20:53:28 -0700

    add User model

--- src/routes/storyRoutes.ts
+++ src/routes/storyRoutes.ts
@@ -1,22 +1,39 @@
 import { Router, Request, Response } from "express";
 import { Story, IStory } from "../models/Story";
+import { User } from "../models/User";
 
 const router = Router();
 
 router.get("/test", async (_req: Request, res: Response) => {
   try {
     const dummy: Partial<IStory> = {
-      title: "🚧 Test Story 🚧",
+      title: "Test Story111111",
       isPublic: true,
       contributors: [],
-      status: "drafting",
+      status: "ongoing",
       createdAt: new Date(),
       updatedAt: new Date(),
       chains: [],
     };
-
+    console.log(":(")
     const created = await Story.create(dummy);
     res.status(201).json(created);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.get("/testUser", async (_req: Request, res: Response) => {
+  try {
+    const dummyUser = {
+      username: "leadDevGavin",
+      password: "ILoveBeingLeadDev123",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+
+    const createdUser = await User.create(dummyUser);
+    res.status(201).json(createdUser);
   } catch (err) {
     console.log(err);
   }
